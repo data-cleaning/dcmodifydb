4c46b1c48a1d9d9ada0a77e917986496e811f0b1
Edwin de Jonge <edwindjonge@gmail.com>
2021-05-04 15:19:49 +0200

    tweaking

---
+++ tests/testthat/dump.sql
@@ -0,0 +1,15 @@
+-- -------------------------------------
+-- Generated with dcmodify, do not edit
+-- dcmodifydb version: 0.1.0.9000
+-- from: 'command-line'
+-- date: 2021-05-04
+-- -------------------------------------
+
+
+
+-- M1:
+-- Nonsense
+-- !!!!
+UPDATE `ds`
+SET 'x' = 1.0
+WHERE `x` > 1.0;
---
+++ tests/testthat/test-sql.R
@@ -0,0 +1,12 @@
+library(dbplyr)
+library(dcmodify)
+
+describe("sql",{
+  d <- tbl_memdb(data.frame(x = 1:2), "ds")
+  it("dumps sql",{
+    m <- modifier(if (x > 1) x <- 1)
+    validate::description(m) <- "Nonsense\n!!!!"
+
+    expect_output_file(dump_sql(m, d), "dump.sql")
+  })
+})
