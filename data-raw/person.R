6912616d28c428b9af88af1d27bbf48a09bee55f
Edwin de Jonge <edwindjonge@gmail.com>
2022-03-09 09:05:45 +0100

    updated documentation

--- data-raw/person.R
+++ data-raw/person.R
@@ -1,9 +1,9 @@
 # create a dataset to be used for testing in dcmodifydb
 
 person <- read.csv(text="
-income, age, gender, year, smokes, cigarrettes
-  2000,  12,      M, 2020,     no,          10
-  2010,  14,      f, 2019,    yes,           4
+income, age, gender, year, smokes, cigarettes
+  2000,  12,      M, 2020,     no,         10
+  2010,  14,      f, 2019,    yes,          4
   2010,  25,      v,   19,     no,
 ", strip.white=TRUE)
 
