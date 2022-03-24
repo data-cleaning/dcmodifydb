f9af14e3136f5da78aef4e907228ef9d9104a741
Edwin de Jonge <edwindjonge@gmail.com>
2022-03-24 12:29:03 +0100

    updating tests

--- tests/testthat/test-duckdb.R
+++ tests/testthat/test-duckdb.R
@@ -1,15 +1,15 @@
 library(dcmodify)
 
 describe("Test against duckdb", {
+
   it("handles the person dataset", {
     skip_on_cran()
 
     skip_if_not_installed("duckdb")
     attachNamespace("duckdb")
-
     con <- DBI::dbConnect(duckdb(), dbdir=":memory:")
-    person_dd <- dplyr::copy_to(con, person)
 
+    person_dd <- dplyr::copy_to(con, person)
     person_sqlite <- dbplyr::tbl_memdb(person)
 
     m <- modifier(.file = system.file("db/corrections.yml", package="dcmodifydb"))
@@ -17,5 +17,29 @@ describe("Test against duckdb", {
     person_c <- modify(person_sqlite, m, copy=FALSE)
     person_dd_c <- modify(person_dd, m, copy=FALSE)
     expect_equal(as.data.frame(person_dd_c), as.data.frame(person_c))
+    DBI::dbDisconnect(con, shutdown=TRUE)
+ })
+
+
+ it("handles the new gear test",{
+   skip_on_cran()
+
+   skip_if_not_installed("duckdb")
+   con <- DBI::dbConnect(duckdb(), dbdir=":memory:")
+
+   mtcars_dd <- dplyr::copy_to(con, mtcars, overwrite=TRUE)
+   mtcars_sqlite <- do.call(dbplyr::memdb_frame, as.list(mtcars))
+   m <- modifier( if (cyl == 6) {
+     new_gear <- 10
+   } else if (cyl == 4) {
+     new_gear <- 25
+   } else {
+     new_gear <- NA
+   })
+
+   mtcars_dd_c <- modify(mtcars_dd, m, copy = FALSE)
+   mtcars_sqlite_c <- modify(mtcars_sqlite, m, copy = FALSE)
+   expect_equal(as.data.frame(mtcars_dd_c), as.data.frame(mtcars_sqlite_c))
+   DBI::dbDisconnect(con, shutdown=TRUE)
  })
 })
