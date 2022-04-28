7fd18699f8fa8cd6fd916ec9fbe2e9d6126d8c20
Edwin de Jonge <edwindjonge@gmail.com>
2022-04-28 11:18:35 +0200

    updated person example

--- data-raw/person.R
+++ data-raw/person.R
@@ -5,6 +5,7 @@ income, age, gender, year, smokes, cigarettes
   2000,  12,      M, 2020,     no,         10
   2010,  14,      f, 2019,    yes,          4
   2010,  25,      v,   19,     no,
+  1010,  65,      M,   20,    yes,
 ", strip.white=TRUE)
 
 usethis::use_data(person, overwrite = TRUE)
